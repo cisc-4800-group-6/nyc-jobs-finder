03875a2e94a9359f6d98811530731cd74e0ab151
KevinTamCS <kevin@kevintamcs.com>
2020-12-09 00:49:14 -0500

    Change API URL to API deployed on Azure App Service

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://nyc-jobs-finder-cisc-4800.azurewebsites.net";
 
 ReactDOM.render(
   <React.StrictMode>
