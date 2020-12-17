4b877b6045970c20f6cb9b1c66ca3929a111fda5
roboterz <40050902+roboterz@users.noreply.github.com>
2020-12-17 04:57:17 -0500

    Update JobSearch.jsx

--- src/views/JobSearch/JobSearch.jsx
+++ src/views/JobSearch/JobSearch.jsx
@@ -30,13 +30,15 @@ const JobSearch = () => {
         // Create a new component for each job and add it to the array of jobs components state
         data.forEach((job) => {
           jobsComponents.push(
-            <Link to={`/job/${job["Job ID"]}`}>
-              <article className="job">
-                <h3>{job["Business Title"]}</h3>
-                <p>{job["Agency"]}</p>
-                <p>Job ID: {job["Job ID"]}</p>
-              </article>
-            </Link>
+            <li>
+              <Link to={`/job/${job["Job ID"]}`}>
+                <article className="job">
+                  <h3>{job["Business Title"]}</h3>
+                  <p>{job["Agency"]}</p>
+                  <p>Job ID: {job["Job ID"]}</p>
+                </article>
+              </Link>
+            </li>
           );
         });
 
