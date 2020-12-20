76f34e7b0cd6f514e57c67e93f5d450e32dbb742
roboterz <40050902+roboterz@users.noreply.github.com>
2020-12-19 20:43:47 -0500

    Update Jobs.jsx

--- src/views/Jobs/Jobs.jsx
+++ src/views/Jobs/Jobs.jsx
@@ -1,19 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { API_URL } from "../../index";
+import { Link, useLocation } from "react-router-dom";
 import "./Jobs.css";
 
 const Jobs = () => {
+  
   const [jobs, setJobs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [hasErrored, setHasErrored] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  
+  // Component State
+
+  const [jobsComponents] = useState([]);
 
   // Get featured jobs from the server when the page loads
   useEffect(() => {
-    fetch(`${API_URL}/jobs`)
+    
+    fetch(`${API_URL}/jobs/`)
       .then((res) => res.json())
       .then((data) => {
-        setJobs(data);
+        data.forEach((job) => {
+          jobsComponents.push(
+          <li>
+            <Link to={`/job/${job["Job ID"]}`}>
+              <article className="job">
+                <h3>{job["Business Title"]}</h3>
+                <p>{job["Agency"]}</p>
+                <p>Job ID: {job["Job ID"]}</p>
+              </article>
+            </Link>
+          </li>
+          );
+        });
+        
         setIsLoading(false);
       })
       .catch((err) => {
@@ -24,28 +44,27 @@ const Jobs = () => {
 
   return (
     <div className="JobSearch">
-      <h1>Featured Jobs</h1>
-
-      {/* Load jobs once the API responds */}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {/* TODO: Display 10 jobs here from fetched data */}
-
-          {/* Hint: The jobs array contains an array of objects containing featured jobs. */}
-          {/* How can we parse them? */}
-        </div>
-      )}
-
-      {/* Display error message if an error occurred */}
-      {hasErrored && (
-        <p style={{ color: "red", fontSize: "2rem" }}>
-          Could not load featured jobs!
-          <br />
-          <strong>{errorMessage}</strong>
-        </p>
-      )}
+      <div id="backtop"><a href="#">TOP</a></div>
+      <h1>All Jobs</h1>
+      <div>
+        <ul className="JobList">
+        {/* Render all the created job components after creating all the jobs */}
+        {isLoading ? (
+          <p>Searching for jobs...</p>
+        ) : jobsComponents.length > 0 ? (
+          jobsComponents 
+        ) : (
+          `No jobs were found in the database.`
+        )}
+        {hasErrored && (
+          <p style={{ color: "red", fontSize: "2rem" }}>
+            Could not search for jobs!
+            <br />
+            <strong>{errorMessage}</strong>
+          </p>
+        )}
+        </ul>
+      </div>
     </div>
   );
 };
